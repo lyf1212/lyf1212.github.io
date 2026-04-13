39579147bfb09a6d3138ffebbe854b711b981f68
lyf1212 <100286723+lyf1212@users.noreply.github.com>
2026-04-13 08:44:27 +0000

    Deploying to gh-pages from @ lyf1212/liyifan.github.io@0c28c3de368d06e4f432bfff3e15149b420f4eb9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "lyf1212/liyifan",
+    "data-repo": "lyf1212/liyifan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
